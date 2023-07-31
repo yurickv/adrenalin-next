763cb28a4551d1494a60c69198be94bf43a658d1
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-07-31 21:19:40 +0300

    header

---
+++ const/index.ts
@@ -0,0 +1,7 @@
+export const NavLinks = [
+    // { href: "/", key: "Main", text: "Головна" },
+    { href: "/blog", key: "Blog", text: "Блог" },
+    { href: "/calcs", key: "Calculators", text: "Калькулятори" },
+    { href: "/services", key: "Services", text: "Послуги" },
+    { href: "/contacts", key: "Contacts", text: "Контакти" },
+];
