21a479a863ac261f0a02ba5d8e69274161afc15f
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-08-15 20:24:07 +0300

    section Contacts

--- app/calcs/page.tsx
+++ app/calcs/page.tsx
@@ -4,13 +4,17 @@ import Image from 'next/image';
 const Calc = () => {
   return (
     <main>
-      <section className="">
-        <div className="container xl:px-[132px] lg:px-[66px]  mx-auto text-center">
-          <h3 className="my-11 text-left">
+      <section className="bg-hero-bg bg-center bg-cover ">
+        <div className="container xl:px-[132px] lg:px-[66px]  py-[100px] mx-auto text-center">
+          <h3 className="my-11 text-left text-white">
             Головна сторінка Adrenalin_gym
             <span className="font-semibold"> &gt; Калькулятори</span>
           </h3>
-          <h1 className="title mb-14">Калькулятори</h1>
+          <h1 className="title mb-14 text-white">Калькулятори</h1>
+        </div>
+      </section>
+      <section>
+        <div className="container xl:px-[132px] lg:px-[66px]  mx-auto text-center mt-6">
           <CalcTitle />
           <Image
             src="/fit-blond.jpg"
