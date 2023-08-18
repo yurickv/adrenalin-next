21399b0b457e47569009a23b0d72b6d4258d080a
vitaliy-kro <krochakv6@gmail.com>
2023-08-18 12:06:27 +0300

    bugfix: change image tag to default browser tag

--- app/api/_helpers/addImageSrcToMarkup.ts
+++ app/api/_helpers/addImageSrcToMarkup.ts
@@ -1,6 +1,6 @@
 export const addImageSrcToMarkup = (markup: string, imageUrl: string) => {
   return markup.replace(
-    /<Image([^>]*)\s+src=(?:"[^"]*"|'[^']*')([^>]*)>/gi,
-    `<Image$1 src="${imageUrl}"$2>`
+    /<img([^>]*)\s+src=(?:"[^"]*"|'[^']*')([^>]*)>/gi,
+    `<img$1 src="${imageUrl}"$2>`
   );
 };
