73da92abf76dc4148924f844dbb883f48d5ff124
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-08-18 21:35:24 +0300

    fix host

--- const/function.ts
+++ const/function.ts
@@ -1,6 +1,6 @@
 export async function getPosts(page: number) {
   try {
-    const posts = await fetch('http://localhost:3000/api/post?limit=12');
+    const posts = await fetch(`http://${process.env.HOST}/api/post?limit=12`);
     return posts.json();
   } catch (error) {
     console.log(error);
@@ -9,7 +9,7 @@ export async function getPosts(page: number) {
 
 export async function getPostForMain(page?: number) {
   try {
-    const posts = await fetch('http://localhost:3000/api/post?limit=3');
+    const posts = await fetch(`http://${process.env.HOST}/api/post?limit=3`);
     return posts.json();
   } catch (error) {
     console.log(error);
