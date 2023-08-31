89315b2bf0ba6af270aa82b828700a87431655fb
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-08-31 16:50:51 +0300

    page 404 for search, small fix

--- components/ButtonSecond.tsx
+++ components/ButtonSecond.tsx
@@ -13,7 +13,7 @@ export const ButtonSecond = ({ route, text, width }: ButtonProps) => {
       className={`border-main border-2 border-solid bg-gradient-to-r 
       hover:from-red-500 hover:to-orange-500 focus:from-red-500 focus:to-orange-500 
       text-mainText hover:text-white focus:text-white
-      rounded-full p-4  text-center block active:bg-primary-700 
+      rounded-full p-4  text-center block 
       hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] 
       shadow-[0_4px_9px_-4px_#3b71ca] ${
         width ? width : 'w-max'
