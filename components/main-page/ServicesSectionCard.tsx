9fa3bb8bf232afbf59bf0b9d79930665ae1adec9
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-09-01 16:06:20 +0300

    Formulas , styles

--- components/main-page/ServicesSectionCard.tsx
+++ components/main-page/ServicesSectionCard.tsx
@@ -10,17 +10,17 @@ export const ServiceCardMain = () => {
         href="/services"
         className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]
            hover:shadow-[rgba(246,_120,_49,_0.29)_0px_9px_20px] hover:bg-orange-100 group
-          dark:bg-neutral-700 pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow"
+          pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow"
       >
         <div className="p-3 rounded-full bg-orange-100 group group-hover:bg-white group-hover:text-main mx-auto inline-block">
           <BarbellForService />
         </div>
 
         <div className="p-6 grow md:flex md:flex-col justify-between">
-          <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 min-h-[50px] group-hover:text-main">
+          <h5 className="mb-2 text-xl font-medium leading-tight text-mainText min-h-[50px] group-hover:text-main">
             Тренування
           </h5>
-          <p className=" text-base text-neutral-600 dark:text-neutral-200 ">
+          <p className=" text-base text-mainText">
             Тренування на професійному обладнанні в просторому тренажерному залі
             Адреналін
           </p>
@@ -30,17 +30,17 @@ export const ServiceCardMain = () => {
         href="/services"
         className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]
            hover:shadow-[rgba(246,_120,_49,_0.29)_0px_9px_20px] hover:bg-orange-100 group
-          dark:bg-neutral-700 pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow max-[767px]:mt-4"
+          pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow max-[767px]:mt-4"
       >
         <div className="p-3 rounded-full bg-orange-100 group group-hover:bg-white group-hover:text-main mx-auto inline-block">
           <TrenerIcon />
         </div>
 
         <div className="p-6 grow">
-          <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 min-h-[50px] group-hover:text-main">
+          <h5 className="mb-2 text-xl font-medium leading-tight text-mainText min-h-[50px] group-hover:text-main">
             Тренування з тренером
           </h5>
-          <p className=" text-base text-neutral-600 dark:text-neutral-200 ">
+          <p className=" text-base text-mainText">
             Передаємо Вам кращий досвід і практики для максимально швидкого
             результату
           </p>
@@ -50,16 +50,16 @@ export const ServiceCardMain = () => {
         href="/services"
         className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]
            hover:shadow-[rgba(246,_120,_49,_0.29)_0px_9px_20px] hover:bg-orange-100 group
-          dark:bg-neutral-700 pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow max-[767px]:mt-4"
+          pt-6 px-3 md:w-[calc((100%-48px)/3)] xl:max-w-[563px] md:min-h-[344px] lg:min-h-[280px] text-center grow max-[767px]:mt-4"
       >
         <div className="p-3 rounded-full bg-orange-100 group group-hover:bg-white group-hover:text-main mx-auto inline-block">
           <PlanIcon />
         </div>
         <div className="p-6 grow">
-          <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 min-h-[50px] group-hover:text-main">
+          <h5 className="mb-2 text-xl font-medium leading-tight text-mainText min-h-[50px] group-hover:text-main">
             План харчування / тренування
           </h5>
-          <p className=" text-base text-neutral-600 dark:text-neutral-200 ">
+          <p className=" text-base text-mainText">
             План харчування / тренування, із врахуванням Вашого віку, ваги і
             мети
           </p>
