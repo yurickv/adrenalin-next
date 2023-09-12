5128c509b7bb2abcb2986fee1fb262d0f7f28d5c
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-09-12 14:45:04 +0300

    toggle light dark mode

---
+++ app/_helpers/useDarkMode.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+type Theme = 'light' | 'dark';
+type SetTheme = (theme: Theme) => void;
+
+function useDarkMode(): [Theme, SetTheme] {
+  const [theme, setTheme] = useState<Theme>(
+    (typeof window !== 'undefined' ? localStorage.theme : 'dark') as Theme
+  );
+  const colorTheme: Theme = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', theme);
+    }
+  }, [theme]);
+
+  return [colorTheme, setTheme];
+}
+
+export default useDarkMode;
