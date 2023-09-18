90ab4f3f31a5f3a783a928b618e74d4da7ccf3f0
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-09-18 15:17:12 +0300

    env

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -15,14 +15,14 @@ export default function ServicesLayout({
     <section>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-RCNV150GJ8"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
       ></Script>
       <Script id="google-analytics">
         {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-RCNV150GJ8')`}
+  gtag('config', '${process.env.GTM_ID}')`}
       </Script>
       {children}
     </section>
