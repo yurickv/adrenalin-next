42734a6e14d354724edd2568b8c8fbfcfc0e5c7c
vitaliy-kro <krochakv6@gmail.com>
2024-01-16 22:46:51 +0200

    feat: update next, change sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,20 @@
+import { MetadataRoute } from 'next';
 import postHttpService from '@/app/_services/post.service';
 
 const URL = 'https://gym-adrenalin.com.ua';
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const { posts } = await postHttpService.getPosts({ limit: 100 });
-  const postsXml = posts.map(({ id, updatedAt }) => ({
+  const postsXml = posts.map(({ id }: { id: string }) => ({
     url: `${URL}/blog/${id}`,
-    lastModified: updatedAt,
+    changeFrequency: 'always',
+    priority: 1,
   }));
 
   const routes = ['', '/portfolio', '/blog'].map(route => ({
     url: `${URL}${route}`,
-    lastModified: new Date().toISOString(),
+    changeFrequency: 'always',
+    priority: 1,
   }));
 
   return [...routes, ...postsXml];
