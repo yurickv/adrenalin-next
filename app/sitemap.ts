e5128a6ca6add4b11fc1eaf909e1ecef8f730bac
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-01-17 10:09:26 +0200

    added const routeSitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next';
 import postHttpService from '@/app/_services/post.service';
+import { routeSitemap } from '@/const/routeSitemap';
 
 const URL = 'https://gym-adrenalin.com.ua';
 
@@ -7,12 +8,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const { posts } = await postHttpService.getPosts({ limit: 100 });
   const postsXml = posts.map(({ id }: { id: string }) => ({
     url: `${URL}/blog/${id}`,
+    lastModified: new Date().toISOString(),
     changeFrequency: 'always',
     priority: 1,
   }));
 
-  const routes = ['', '/portfolio', '/blog'].map(route => ({
+  const routes = routeSitemap.map(route => ({
     url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
     changeFrequency: 'always',
     priority: 1,
   }));
