e3ad195aab1b62494d13c2df1beca35d9a007797
Teslyuk Yurii <yurickv@gmail.com>
2025-05-16 12:39:31 +0300

    update SurveyPopup

--- components/ButtonSecond.tsx
+++ components/ButtonSecond.tsx
@@ -11,8 +11,7 @@ export const ButtonSecond = ({ route, text, width }: ButtonProps) => {
     <Link
       href={route}
       className={`bg-orange-100 hover:bg-orange-200 dark:bg-[#a3a3a3] dark:hover:bg-[#d4d4d4]
-      text-mainTitle
-      rounded-xl p-4  text-center block 
+      text-mainTitle rounded-xl p-4 text-center block 
        ${width ? width : 'w-max'} transition-all duration-150`}
     >
       {text}
--- components/calcs-page/SurveyPopup.tsx
+++ components/calcs-page/SurveyPopup.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { ButtonSecond } from '../ButtonSecond';
+import Link from 'next/link';
 
 const SurveyPopup = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -28,7 +29,7 @@ const SurveyPopup = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center">
-        <p className="text-lg font-semibold">
+        {/* <p className="text-lg font-semibold">
           💡 Ми покращуємо наші інструменти!
         </p>
         <p className="mt-2 text-gray-700">
@@ -39,7 +40,23 @@ const SurveyPopup = () => {
           route="/questionary"
           text="Пройти опитування 📝"
           width="mx-auto mt-4 w-full md:w-[284px]"
-        />
+        /> */}
+        <p className="text-lg font-semibold">
+          🤖 Хочете персональне меню на день?
+        </p>
+        <p className="mt-2 text-gray-700">
+          Наш безкоштовний Telegram-бот <strong>@Nutri_day_bot</strong> допоможе
+          вам скласти денний раціон харчування, враховуючи ваші цілі та
+          вподобання.
+        </p>
+        <Link
+          href="https://t.me/Nutri_day_bot"
+          target="_blank"
+          className={`bg-orange-100 hover:bg-orange-200 dark:bg-[#a3a3a3] dark:hover:bg-[#d4d4d4]
+      text-mainTitle rounded-xl p-4 text-center block mx-auto mt-4 w-full md:w-[284px] transition-all duration-150`}
+        >
+          Спробувати бота в Telegram 🚀
+        </Link>
         <button
           onClick={handleClose}
           className="mt-3 text-gray-500 hover:text-gray-700"
